d63c0fe45d0d2c5136e3ae0b7649c9ac92ed5dde
Emerson Coskey <56370779+EmersonCoskey@users.noreply.github.com>
2021-09-25 00:17:24 -0700

    setup state hooks

--- src/hooks/useTimedMedia.ts
+++ src/hooks/useTimedMedia.ts
@@ -1,20 +1,43 @@
-import React, { useState } from 'react';
+import { MutableRefObject, useEffect, useReducer, useRef, useState } from 'react';
+import AVLIterator, { IteratorDirection } from '../bst/AVLIterator';
+//import useAnimationFrame from 'use-animation-frame';
 import AVLTree from '../bst/AVLTree';
 import compareNums from '../util/compareNums';
+import defaulted from '../util/Defaulted';
 import { KVP } from '../util/KeyValuePair';
 
-export interface timedMediaConfig {
+export interface TimedMediaConfig {
     autoplay?: boolean,
-    playDirection?: 'forward' | 'backward',
-
+    playDir?: 'forward' | 'reverse',
+    startTime?: number,
 }
 
-export interface timedMediaConfigDefaults {
-    autoplay: false,
-    playDirection: 'forward',
+export default function useTimedMedia<E>(config: TimedMediaConfig, items?: Iterable<KVP<number, E[]>> | null | undefined) {
+    const [ timeline, setTimeline ] = useState<AVLTree<number, E>>(() => new AVLTree(compareNums, items));
 
+    const [ playing, setPlaying ] = useState<boolean>(config.autoplay ?? true);
+    const [ playDir, setPlayDir ] = useState<'forward' | 'reverse'>(config.playDir ?? 'forward');
+    const [ maxTime, setMaxTime ] = useState<number>();
+    const [ startTime, setStartTime] = useState<number>(config.startTime /* TODO: Clamp to [0, maxTime] */?? 0);
+
+    // TODO:  const [ handlers, addHandler ] = useReducer( etc etc )
+
+    
+
+    const playHead = useRef<AVLIterator<number, E>>(timeline.entries(/* among us (finish this declaration) */));
+
+    useEffect(() => { //reset playHead when direction changes
+
+    }, [playDir]);
+
+    /*useAnimationFrame(() => {
+
+    }, [state.playing, state.playDirection]); */
+
+    return {
+        setPlaying: setPlaying,
+        setPlayDir: setPlayDir,
+        setMaxTime: setMaxTime,
+        setStartTime: setStartTime,
+    };
 }
-
-export default function useTimedMedia<E>(config: timedMediaConfig, items?: Iterable<KVP<number, E[]>> | null | undefined) {
-    const [timeline, setTimeline] = useState(() => new AVLTree(compareNums, items));
-} 
