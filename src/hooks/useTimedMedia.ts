9e7af603653c7dfed00bcb59a0304ffd5cbc065a
Emerson Coskey <56370779+EmersonCoskey@users.noreply.github.com>
2021-10-02 23:07:45 -0700

    Now to testing phase of rework
    finished?????? rework, should have all the same functionality, except no write functionality on API object

--- src/hooks/useTimedMedia.ts
+++ src/hooks/useTimedMedia.ts
@@ -1,13 +1,10 @@
-import { useEffect, useReducer, useRef, useState } from 'react';
-import AVLIterator from '../bst/AVLIterator';
+import { useState } from 'react';
 import useAnimationFrame from 'use-animation-frame';
 import AVLTree from '../bst/AVLTree';
-import clamp from '../util/clamp';
 import compareNums from '../util/compareNums';
 import type { KVP }  from '../util/KeyValuePair';
 import EventBus, { CallbackFromTuple } from '../util/EventBus';
 import AVLNode from '../bst/AVLNode';
-import { getIterableFirst } from '../util/Iterables';
 
 export interface PlaybackState {
     playing: boolean,
@@ -30,10 +27,10 @@ export interface TimedMediaAPI<E> {
     readonly unsubscribe: EventBus<TimedMediaEvents<E>>['unsubscribe'],
     readonly clear: EventBus<TimedMediaEvents<E>>['clear'],
 
-    playing: boolean,
-    playDirection: 'forward' | 'reverse',
-    time: number,
-    length: number,
+    readonly playing: boolean,
+    readonly playDirection: 'forward' | 'reverse',
+    readonly time: number,
+    readonly length: number,
 }
 
 export default function useTimedMedia<E>(config: Partial<PlaybackState> & { seek?: boolean }, items?: Iterable<KVP<number, E[]>> | null | undefined): TimedMediaAPI<E> {
@@ -61,9 +58,30 @@ export default function useTimedMedia<E>(config: Partial<PlaybackState> & { seek
 
     addToPlaybackState(config);
 
-    if ((config.playing && config.playing !== playing) || (config.playDir && config.playDir !== playDir)) { handleSetPlaying(); }
-    if ((config.time && config.time !== time) || config.seek) { handleSeek(); }
-    if (config.length && config.length !== length) { handleSetLength(); }
+    if (config.playing && config.playing !== playing) { 
+        resetPlayHead();
+        handlers.dispatch('togglePlayback', playing, time);
+    }
+
+    if (config.playDir && config.playDir !== playDir) { 
+        resetPlayHead(); 
+        handlers.dispatch('togglePlayDirection', playDir);
+    }
+
+    if ((config.time && config.time !== time) || config.seek) { 
+        resetPlayHead();
+        handlers.dispatch('seek', time);
+    }
+
+    if (config.length && config.length !== length) { 
+        if (time > length)  { // if new maxTime is lower than the time the playhead is at
+            addToPlaybackState({
+                playing: playDir === 'reverse',
+                time: length,
+            });
+        }
+        handlers.dispatch('setLength', length); 
+    }
 
     // --- --- ---[ function declarations for handling changing state, and timer function ]--- --- ---
 
@@ -72,52 +90,26 @@ export default function useTimedMedia<E>(config: Partial<PlaybackState> & { seek
         setPlaybackState({...playbackState, ...newState});
     }
 
-    function handleSetPlaying(): void { //reset playHead when direction / startTime changes, and set new playHead and currentTime accordingly.
-        playHead.current = timeline.entries(playDir === 'forward' ? 'ascending' : 'descending', startTime);
-        playHead.current.next();
-        currentTime.current = startTime;
-
-        // --- --- ---[ Handlers ]--- --- ---
-        handlers.dispatch('togglePlayDirection', playDir);
-        handlers.dispatch('seek', startTime);
-    }, [timeline, playDir, startTime, handlers]);
-
-    function handleSeek(): void { //update startTime to current place on timeline I guess? can probably do it dual-edge, it shouldn't be too slow
-        setStartTime(currentTime.current);
-
-        // --- --- ---[ Handlers ]--- --- ---
-        handlers.dispatch('togglePlayback', playing, currentTime.current);
-    }, [playing, handlers]); 
-
-    function handleSetLength(): void { // if new maxTime is lower than the time the playhead is at, handle accordingly and clamp current time
-        if (maxTime < currentTime.current)  { // if new maxTime is lower than the time the playhead is at
-            setPlaying(playDir === 'reverse'); // * if playing in reverse, keeps playing and seeks to new maxTime
-            setStartTime(maxTime);
-        }
-
-        // --- --- ---[ Handlers ]--- --- ---
-        handlers.dispatch('setMaxTime', maxTime);
-    }, [maxTime, playDir, handlers]);
+    function resetPlayHead(): void {
+        setPlayHead(timeline.root?.search(time, playDir === 'forward' ? 'closest-max' : 'closest-min'));
+    }
 
     useAnimationFrame(({ delta }) => {
         if (!playing) { return; }
 
         //update currentTime, if we've passed an event node than call handlers and update playHead to next.
-        const newTime = 
-            playDir === 'forward' ? 
-            time + (delta * 1000) : 
-            time - (delta * 1000) ;
+        const newTime = playDir === 'forward' ? time + (delta * 1000) : time - (delta * 1000);
 
         addToPlaybackState({ time: newTime });
 
         const hasNextEvent = playHead !== undefined && (playDir === 'forward' ? length > playHead.key : playHead.key > 0);
         const hasOverflowed = playDir === 'forward' ? length <= time : time <= 0;
 
-        const nextEvent = playDir === 'forward' ? playHead ?? {key: length, value: []} : playHead ?? {key: 0, value: []};
-        const timeUntilNext = playDir === 'forward' ? nextEvent.key - currentTime.current : currentTime.current - nextEvent.key;
+        const nextEvent = playHead ?? playDir === 'forward' ? {key: length, value: []} : {key: 0, value: []};
+        const timeUntilNext = playDir === 'forward' ? nextEvent.key - time : time - nextEvent.key;
 
         if (hasNextEvent && timeUntilNext <= 0) {
-            playHead.current.next();
+            setPlayHead(playDir === 'forward' ? playHead.successor : playHead.predecessor);
 
             // --- --- ---[ Handlers ]--- --- ---
             handlers.dispatch('media', nextEvent.value, nextEvent.key);
@@ -131,23 +123,16 @@ export default function useTimedMedia<E>(config: Partial<PlaybackState> & { seek
             // --- --- ---[ Handlers ]--- --- ---
             handlers.dispatch('end');
         }
-    }, [playing, playDir, startTime, handlers]);
+    }, [playing, playDir, handlers]);
 
-    return { //? what to return for API?
+    return {
         on: <K extends keyof TimedMediaEvents<E>>(event: K, callback: CallbackFromTuple<TimedMediaEvents<E>[K]>) => handlers.on(event, callback),
         unsubscribe: <K extends keyof TimedMediaEvents<E>>(event: K, target: number) => handlers.unsubscribe(event, target),
         clear: () => handlers.clear(),
 
         get playing() { return playing; },
-        set playing(playing: boolean) { setPlaying(playing); },
-
         get playDirection() { return playDir; },
-        set playDirection(dir: 'forward' | 'reverse') { setPlayDir(dir); },
-
-        get time() { return currentTime.current; },
-        set time(time: number) { setStartTime(time); },
-
-        get length() { return maxTime; },
-        set length(newLength: number) { setMaxTime(newLength); },
+        get time() { return time; },
+        get length() { return length; },
     };
 }
